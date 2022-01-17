5a694454f7f1bdeb3f9dea734b70e2f43bd8d898
lichkingvamvoo <lich_king@vamvoo.io>
2022-01-18 02:56:11 +0900

    Update api url

--- src/streams/tokenPrice.js
+++ src/streams/tokenPrice.js
@@ -3,7 +3,7 @@ import { map, mergeMap, switchMap, tap } from "rxjs/operators";
 import { tokenList } from "../constants/tokens";
 
 export const fetchKlayswapInfo$ = from(
-  fetch("https://api.birkosully.com/api/klaytn/klayswapInfo").then((res) => res.json())
+  fetch("https://api.kltalchemy.com/klay/ksInfo").then((res) => res.json())
 )
 
 export const tokenPrices$ = new BehaviorSubject({})
