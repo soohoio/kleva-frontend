52c1745a488ec638630d4724e70a9f884b748426
lichkingvamvoo <lich_king@vamvoo.io>
2022-07-01 15:34:23 +0900

    Add CompletedModal component

---
+++ src/components/common/CompletedModal.js
@@ -0,0 +1,49 @@
+import React, { Component, Fragment, createRef } from 'react'
+import cx from 'classnames'
+import { Subject, merge, of } from 'rxjs'
+import { takeUntil, tap, debounceTime } from 'rxjs/operators'
+
+import './CompletedModal.scss'
+import Modal from './Modal'
+
+class CompletedModal extends Component {
+
+  destroy$ = new Subject()
+  
+  componentDidMount() {
+    merge(
+      of(true),
+    ).pipe(
+      debounceTime(1),
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.forceUpdate()
+    })
+    
+  }
+  
+  componentWillUnmount() {
+    this.destroy$.next(true)
+  }
+    
+  render() {
+    const { children, menus } = this.props
+    
+    return (
+      <Modal className="CompletedModal">
+        <div className="CompletedModal__content">
+          {children}
+        </div>
+        <div className="CompletedModal__menus">
+          {menus && menus.map(({ title, onClick }) => {
+            return (
+              <div onClick={onClick} className="CompletedModal__menuItem">{title}</div>
+            )
+          })}
+        </div>
+      </Modal>
+    )
+  }
+}
+
+export default CompletedModal
