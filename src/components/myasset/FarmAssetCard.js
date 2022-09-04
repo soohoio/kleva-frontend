d38f9c875845a4a855d66cec8b861b6b6b81827d
lichkingvamvoo <lich_king@vamvoo.io>
2022-09-04 12:41:01 +0900

    QA: Fix KLEV-74, KLEV-76

--- src/components/myasset/FarmAssetCard.js
+++ src/components/myasset/FarmAssetCard.js
@@ -248,6 +248,7 @@ class FarmAssetCard extends Component {
                 "FarmAssetCard__gauge--red": debtRatio !== 0 && debtRatio > 70,
               })}
             />
+            <img src="/static/images/exported/warn-mark.svg" className="FarmAssetCard__warnMark" />
           </div>
           <p className="FarmAssetCard__debtRatioDescription">{I18n.t('myasset.farming.debtRatio.description')}</p>
           <div className="FarmAssetCard__buttons">
